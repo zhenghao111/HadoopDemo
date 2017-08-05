5db5dc2fdc24666d2224ad3c57f46d37c6e308e4
zhenghao <zhenghao111@qq.com>
2017-08-05 11:26:38 +0800

    edit

---
+++ Demo1/src/main/java/com/zhenghao/mapreduce/v2/MaxTemperatureReducer.java
@@ -0,0 +1,23 @@
+package com.zhenghao.mapreduce.v2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+//        super.reduce(key, values, context);
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(value.get(), maxValue);
+        }
+
+        context.write(key, new IntWritable(maxValue));
+
+
+    }
+}
--- Demo1/src/main/java/com/zhenghao/mapreduce/v2/NcdcRecordParser.java
+++ Demo1/src/main/java/com/zhenghao/mapreduce/v2/NcdcRecordParser.java
@@ -1,8 +1,41 @@
 package com.zhenghao.mapreduce.v2;
 
+import org.apache.hadoop.io.Text;
+
 public class NcdcRecordParser {
 
     public static final int MISSING_TEMPERATURE = 9999;
 
-    private String
+    private String year;
+    private int airTemperature;
+    private String quality;
+
+    public void parser(String record) {
+        year = record.substring(15, 19);
+        String airTempeatureString;
+        if (record.charAt(87) == '+') {
+            airTempeatureString = record.substring(88, 92);
+        } else {
+            airTempeatureString = record.substring(87, 92);
+        }
+
+        airTemperature = Integer.parseInt(airTempeatureString);
+        quality = record.substring(92, 93);
+    }
+
+    public void parser(Text record) {
+        parser(record.toString());
+    }
+
+    public boolean isValidTemperature() {
+        return  airTemperature != MISSING_TEMPERATURE && quality.matches("[01459]");
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getAirTemperature() {
+        return airTemperature;
+    }
 }
