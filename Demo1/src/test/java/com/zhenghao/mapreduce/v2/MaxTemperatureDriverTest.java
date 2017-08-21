b7ae28ca4dac3f786876ba25ce52abcfd5375ce2
zhenghao <zhenghao111@qq.com>
2017-08-22 06:33:22 +0800

    edit

--- Demo1/src/test/java/com/zhenghao/mapreduce/v2/MaxTemperatureDriverTest.java
+++ Demo1/src/test/java/com/zhenghao/mapreduce/v2/MaxTemperatureDriverTest.java
@@ -1,12 +1,16 @@
 package com.zhenghao.mapreduce.v2;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.PathFilter;
+import org.apache.hadoop.fs.*;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class MaxTemperatureDriverTest {
@@ -34,11 +38,33 @@ public class MaxTemperatureDriverTest {
         MaxTemperatureDriver driver = new MaxTemperatureDriver();
         driver.setConf(conf);
         int exitCode = driver.run(new String[]{input.toString(), output.toString()});
-        assertThat(exitCode, is(0));
+//        assertThat(exitCode, is(0));
+
+        checkOutput(conf, output);
     }
 
-    private void checkOutput(Configuration conf, Path output) {
+    private void checkOutput(Configuration conf, Path output) throws IOException {
+        FileSystem fs = FileSystem.get(conf);
+        Path[] outputFiles = FileUtil.stat2Paths(fs.listStatus(output, new OutputLogFilter()));
+        assertThat(outputFiles.length, is(1));
 
+        BufferedReader actual = asBufferedReader(fs.open(outputFiles[0]));
+        BufferedReader expected = asBufferedReader(getClass().getResourceAsStream("/expected.txt"));
+
+        String expectedLine;
+        while ((expectedLine = expected.readLine()) != null) {
+            assertThat(actual.readLine(), is(expectedLine));
+        }
+
+        assertThat(actual.readLine(), nullValue());
+
+        actual.close();
+        expected.close();
+
+    }
+
+    private BufferedReader asBufferedReader(InputStream in) {
+        return new BufferedReader(new InputStreamReader(in));
     }
 
 
