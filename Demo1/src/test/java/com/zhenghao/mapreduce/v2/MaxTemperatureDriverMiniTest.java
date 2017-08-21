6b1ad8380fc429c5dda6d2692b6cae05422860a5
zhenghao <zhenghao111@qq.com>
2017-08-22 07:03:04 +0800

    edit

---
+++ Demo1/src/test/java/com/zhenghao/mapreduce/v2/MaxTemperatureDriverMiniTest.java
@@ -0,0 +1,31 @@
+package com.zhenghao.mapreduce.v2;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+import org.apache.hadoop.mapred.ClusterMapReduceTestCase;
+
+public class MaxTemperatureDriverMiniTest extends ClusterMapReduceTestCase {
+
+
+
+    public static class OutputLogFilter implements PathFilter {
+
+        @Override
+        public boolean accept(Path path) {
+            return !path.getName().startsWith("_");
+        }
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        if (System.getProperty("test.build.data") == null) {
+            System.setProperty("test.build.data", "tmp");
+        }
+
+
+        super.setUp();
+    }
+
+    public void testName() throws Exception {
+    }
+}
